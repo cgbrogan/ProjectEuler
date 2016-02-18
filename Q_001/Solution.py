20caa85f69446c2f17850c7487245501d0c3be6a
Connor Brogan <cgbrogan36@gmail.com>
2016-02-18 15:14:27 -0500

    Update Solution.py

--- Q_001/Solution.py
+++ Q_001/Solution.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python
+
+s = []
+for val in range(0,1000):
+    if (val % 3 == 0) and (val not in s):
+        s.append(val)
+    elif (val % 5 == 0) and (val not in s):
+        s.append(val)
+    else:
+        continue
+
+print sum(s)
